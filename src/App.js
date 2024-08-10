5fa7cf80680dbfe4db2be8469df7bfc9fdf1ffff
JanineDavis <122249027+JanineDavis@users.noreply.github.com>
2024-08-10 14:34:58 +1000

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <WeatherSearch />
-      </header>
-      <p>
-        <a href="https://github.com/JanineDavis/weather-react" target="_blank">
-          Git Repo
-        </a>
-      </p>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <WeatherSearch />
+        </header>
+        <p>
+          <a
+            href="https://github.com/JanineDavis/weather-react"
+            target="_blank"
+          >
+            Git Repo
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
