ca208d026758d5aeca3401c283f3eec467bf5789
JanineDavis <122249027+JanineDavis@users.noreply.github.com>
2024-08-10 14:20:49 +1000

    updated link

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import WeatherSearch from "./Weather";
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
         <WeatherSearch />
       </header>
-      <a href="https://github.com/JanineDavis/weather-react">Git Repo</a>
+      <p>
+        <a href="https://github.com/JanineDavis/weather-react" target="_blank">
+          Git Repo
+        </a>
+      </p>
     </div>
   );
 }
-
-export default App;
