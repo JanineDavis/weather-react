a11f4d7a08b8e3fb68372c1346138a5d3cc63e44
JanineDavis <122249027+JanineDavis@users.noreply.github.com>
2024-08-29 15:03:51 +1000

    Added forecast Day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
   function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
     console.log(response.data);
   }
-  let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div classNAme="col">
-          <div className="WeatherForecast-day">Thur</div>
-          <div className="WeatherIcon">Icon</div>{" "}
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-min">19° </span>
-            <span className="WeatherForecast-temperature-max">21°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "445905dadb3d2b0c6f1b916c9d0e3860";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
